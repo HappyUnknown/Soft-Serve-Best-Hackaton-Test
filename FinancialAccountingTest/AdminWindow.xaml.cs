2f81ea998c0f2a7b1acfb335977ce0cea3a0bf70
HappyUnknown <39666812+HappyUnknown@users.noreply.github.com>
2023-05-14 18:57:06 +0300

    Minimal grid interaction

---
+++ FinancialAccountingTest/AdminWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FinancialAccountingTest
+{
+    /// <summary>
+    /// Interaction logic for AdminWindow.xaml
+    /// </summary>
+    public partial class AdminWindow : Window
+    {
+        public AdminWindow()
+        {
+            InitializeComponent();
+            List<FinancialLog> logs = new List<FinancialLog>() {
+                new FinancialLog() { Id = 1, Date = DateTime.Now, Description = "Sample Text", LogType = FLType.Income, Value = 123 },
+                new FinancialLog() { Id = 1, Date = DateTime.Now, Description = "Sample Text", LogType = FLType.Income, Value = 123 },
+                new FinancialLog() { Id = 1, Date = DateTime.Now, Description = "Sample Text", LogType = FLType.Income, Value = 123 }
+            };
+            dgLogs.ItemsSource = logs;
+        }
+    }
+}
---
+++ FinancialAccountingTest/FinancialLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialAccountingTest
+{
+    class FinancialLog
+    {
+        public int Id { get; set; }
+        public double Value { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public FLType LogType { get; set; }
+    }
+}
